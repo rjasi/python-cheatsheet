e347eefb65751a19915171fe6f00f8fd9ee12d9b
rjasi <venusara@gmail.com>
2020-08-26 20:15:07 -0400

    comma placements

---
+++ misc/commas.py
@@ -0,0 +1,19 @@
+
+
+def comma_placement():
+    words = [
+        "red",
+        "blue",
+        "green" # <- missing comma
+        "yellow"]
+
+    # above is legit, will print ['red', 'blue', 'greenyellow']
+
+    # to avoid, just add comma after all items
+
+    words = [
+        "red",
+        "blue",
+        "green",
+        "yellow", # last item has comma but legit syntax
+        ]
